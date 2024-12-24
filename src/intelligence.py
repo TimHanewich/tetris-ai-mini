a02de2357791f170b9f9090347a22e72646fde73
Tim Hanewich <tahanewich@live.com>
2024-12-24 17:02:07 -0500

    Reduced learning rate

--- src/intelligence.py
+++ src/intelligence.py
@@ -27,7 +27,7 @@ class TetrisAI:
 
             # construct the model
             self.model = keras.Model(inputs=input_board, outputs=output)
-            self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.01), loss="mse")
+            self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.003), loss="mse")
 
     def save(self, path:str) -> None:
         """Saves the keras model to file"""
