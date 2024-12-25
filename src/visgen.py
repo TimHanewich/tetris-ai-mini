9b5548e0d25ac72234fba9ed78f522be0f7b93ca
Tim Hanewich <tahanewich@live.com>
2024-12-25 07:18:53 -0500

    Start with frames, end with frame

--- src/visgen.py
+++ src/visgen.py
@@ -3,17 +3,46 @@ import tetris
 import intelligence
 import representation
 import random
+import os
 
+### SETTINGS ###
 model_save_path = r"C:\Users\timh\Downloads\tah\tetris-ai-mini\checkpoints\checkpoint16.keras"
 images_save_folder = r"C:\Users\timh\Downloads\tah\tetris-ai-mini\game_images"
+################
+
+if os.path.exists(images_save_folder) == False:
+    os.mkdir(images_save_folder)
 
 print("Loading model...")
 tai = intelligence.TetrisAI(model_save_path)
 
+# variables for tracking
+onFrame:int = 0
 onGame:int = 1
-gs = tetris.GameState()
-for i in range(0, 100):
+
+def next_save_path() -> str:
+    global onFrame
+    path = images_save_folder + "\\" + f"{onFrame:06d}" + ".png"
+    onFrame = onFrame + 1
+    return path
+
+# generate!
+gs:tetris.GameState = tetris.GameState()
+for i in range(0, 60):
+
     print("On move " + str(i) + "... ")
+
+    # create next game if needed
+    if gs == None or gs.over():
+        print("We need to generate a new game!")
+        gs = tetris.GameState()
+        if random.random() <= 0.95:
+            gs.randomize()
+        onGame = onGame + 1
+
+        # generate a few frames of this starting space so that way the observer can see the ending spot.
+        for _ in range(0, 8):
+            visuals.genimg(gs, next_save_path(), None, onGame)
 
     # get move
     predictions:list[float] = tai.predict(representation.BoardState(gs))
@@ -27,13 +56,9 @@ for i in range(0, 100):
     highlight:tuple[int, int] = (cds[shift], shift)
 
     # save image of game
-    path = images_save_folder + "\\" + f"{i:06d}" + ".png"
-    visuals.genimg(gs, path, highlight, onGame)
-    
-    # game over?
+    visuals.genimg(gs, next_save_path(), highlight, onGame)
+
+    # if that last move the model played ended the game (now 16 squares are filled), generate a few more frames
     if gs.over():
-        print("Game over! Going to next game.")
-        gs = tetris.GameState()
-        if random.random() <= 0.95:
-            gs.randomize()
-        onGame = onGame + 1
+        for _ in range(0, 5):
+            visuals.genimg(gs, next_save_path(), highlight, onGame)
